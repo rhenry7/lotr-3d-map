36e0ddd0d94502442b1361b91379fee30e2fd57c
Ramone Henry <ramoneh94@gmail.com>
2026-01-31 14:23:21 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // For GitHub Pages or subpath hosting, set base to your repo name, e.g. base: '/lotr-3d-map/'
-  base: '/',
+  base: '/lotr-3d-map/',
 });
